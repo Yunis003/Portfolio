9988968d302e4956e3039f127c8021ed4b8aba0b
Yunis003 <hi.yunisqulamov@gmail.com>
2025-02-08 00:16:39 +0400

    Work Experience Animations

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 window.addEventListener("scroll", function () {
     let myBox = document.querySelector('.background-circle');
+    let experienceBox = this.document.querySelectorAll('.experience-box');
     let triggerPoint = 350;
     if (window.scrollY > triggerPoint) {
       myBox.classList.add("shadow");
+      experienceBox.forEach((box)=>box.classList.add("boxes"));
     } else {
       myBox.classList.remove("shadow");
+      experienceBox.forEach((box)=>box.classList.remove("boxes"));
     }
   });
   
